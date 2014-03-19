ee0ff50f9c953d3010b2d58f721bd646177eb5af
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-03-18 20:43:25 -0400

    Not sure why modifying firstChild.data doesn'ttrigger the MutationObserver

--- button_listen.js
+++ button_listen.js
@@ -3,11 +3,15 @@ var button = document.getElementById("mybutton");
 var observer = new MutationObserver(function(mutations) {
   mutations.forEach(function(mutation) {
     console.log(mutation.type);
+    console.log(mutation.target.firstChild.data);
+    console.log(mutation.target.style["background-color"]);
+    mutation.target.style["background-color"] = "lightred";
+    console.log(mutation.target.style["background-color"]);
   });
 });
 var config = {attributes:true, childList:true, characterData:true};
 observer.observe(button, config);
 
-button.style["background-color"] = "lightblue";
+button.firstChild.data = "click me !!!";
 
 //observer.disconnect();
