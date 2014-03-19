67e7c765e0d4e229ca8f6dd5bce19fe372ae1268
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-03-19 14:12:41 -0400

    Toggling color of the div causes mutation observer to fire.

--- button_listen.js
+++ button_listen.js
@@ -1,17 +1,12 @@
-var button = document.getElementById("mybutton");
 // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
 var observer = new MutationObserver(function(mutations) {
   mutations.forEach(function(mutation) {
-    console.log(mutation.type);
-    console.log(mutation.target.firstChild.data);
-    console.log(mutation.target.style["background-color"]);
-    mutation.target.style["background-color"] = "lightred";
-    console.log(mutation.target.style["background-color"]);
+    console.log("style background-color = " + mutation.target.style["background-color"]);
   });
 });
 var config = {attributes:true, childList:true, characterData:true};
-observer.observe(button, config);
+var square = document.getElementById("square");
+observer.observe(square, config);
 
-button.firstChild.data = "click me !!!";
 
 //observer.disconnect();
