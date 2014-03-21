13c1d076879a22196c4f4234eb328814433d41df
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-03-20 22:17:17 -0400

    Can't seem to fade the color of the element border inside the MutationSummary callback without firing the callback again. Thought MutationSummary was supposed to suspend further callbacks until the current callback is finished. wtf

--- button_listen.js
+++ button_listen.js
@@ -1,11 +1,33 @@
+// Yanked from "Javascript The Good Parts" p. 38
+var fade = function(node) {
+  var level = 1;
+  var step = function() {
+    var hex = level.toString(16);
+    node.style["border-color"] = '#FFFF' + hex + hex;
+    if (level < 15) {
+      level += 1;
+      setTimeout(step, 1000);
+    }
+  };
+  setTimeout(step, 1000);
+};
+
 var styleReactor = function(summaries) {
-  console.log(summaries);
   mutationSummary = summaries[0];
-  console.log(mutationSummary);
-  mutationSummary.attributeChanged.forEach(function(changedElement) {
-    console.log(changedElement);
-    changedElement.style["border"] = "5px solid";
-    changedElement.style["border-color"] = "red";
+  mutationSummary.valueChanged.forEach(function(changedElement) {
+    console.log("styleReactor");
+    changedElement.style["border"] = "3px solid";
+    var level = 1;
+    var intervalId = setInterval(function() {
+      var hex = level.toString(16);
+      if (level < 15) {
+        changedElement.style["border-color"] = '#FFFF' + hex + hex;
+        level += 1;
+      } else {
+        clearInterval(intervalId);
+      }
+    }, 1000);
+    //changedElement.style["border"] = "0px";
   });
 };
 
@@ -13,6 +35,6 @@ var styleReactor = function(summaries) {
 var observer = new MutationSummary({
   callback: styleReactor,
   queries: [
-    {all: true}
+    {attribute: "style"}
   ]
 });
