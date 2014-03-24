d1d54b88f1d4e96b2b25c6be28972897315e2411
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-03-24 17:24:19 -0400

    Use a css transition. Still causes a recursion chain though :(

--- button_listen.js
+++ button_listen.js
@@ -1,31 +1,17 @@
 var styleReactor = function(summaries) {
+  console.log("event fired");
   mutationSummary = summaries[0];
   mutationSummary.valueChanged.forEach(function(changedElement) {
-    console.log("styleReactor");
-    if (changedElement.getAttribute("myUpdateQ")) return;
-    var myAttr = document.createAttribute("myUpdateQ");
-    myAttr.value = true;
-    changedElement.setAttributeNode(myAttr);
-    console.log("element name = " + changedElement.id);
-    console.log("myUpdateQ = " + changedElement.getAttribute("myUpdateQ"));
     var oldBorder = changedElement.style["border"];
-    var oldColor = changedElement.style["border-color"];
-    changedElement.style["border"] = "3px solid";
-    var level = 1;
-    var fade = function() {
-      console.log("level = " + level);
-      var hex = level.toString(16);
-      //changedElement.style["border-color"] = '#FFFF' + hex + hex;
-      if (level == 14) {
-        myAttr.value = false;
-        changedElement.setAttributeNode(myAttr);
-        clearInterval(intId);
-      }
-      level ++;
-    };
-    setInterval(fade, 1000);
-    changedElement.style["border"] = oldBorder;
-    changedElement.style["border-color"] = oldColor;
+    console.log(oldBorder);
+
+    changedElement.style["border"] = "thick solid yellow";
+    changedElement.style.webkitTransition = "border-color 2s";
+
+    setTimeout(function() {
+      changedElement.style["border"] = oldBorder;
+    }, 2000);
+
   });
 };
 
