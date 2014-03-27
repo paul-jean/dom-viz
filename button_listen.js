d0ef56ddc183e3a1f2db2d8ff8b3651b904f5ab9
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-03-27 14:27:59 -0400

    Add more event types.

--- button_listen.js
+++ button_listen.js
@@ -1,50 +1,79 @@
-var effect = 'wobble';
+var effect = 'highlight';
 
-var styleReactor = function(summaries) {
-  mutationSummary = summaries[0];
-  mutationSummary.valueChanged.forEach(function(changedElement) {
-    // If I'm currently changing this element, don't do anything:
-    if (changedElement.className && changedElement.className.match(/animating/))
-      return;
+var reactorDispatch = function(type) {
+  if (type == 'style') return highlightElement;
+  else if (type == 'add') return highlightElement;
+  else if (type == 'character') return highlightElement;
+  else return undefined;
+};
 
-    // Add my css keyframes tag to the element's classes:
-    if (!changedElement.className.match(effect))
-      changedElement.className += changedElement.className ? ' ' + effect : effect;
-    // Add a solid border so my highlight animation can style it:
-    if (!changedElement.className.match('framed'))
-      changedElement.className += ' framed';
+var highlightElement = function(changedElement) {
+  // If I'm currently changing this element, don't do anything:
+  if (changedElement.className && changedElement.className.match(/animating/))
+    return;
 
-    // When the animation starts ...
-    var beginAnimListen = function() {
-      // ... add a class to flag it as currently animating:
-      changedElement.className += changedElement.className ? ' animating': 'animating';
-    };
+  // Add my css keyframes tag to the element's classes:
+  if (!changedElement.className.match(effect))
+    changedElement.className += changedElement.className ? ' ' + effect : effect;
+  // Add a solid border so my highlight animation can style it:
+  if (!changedElement.className.match('framed'))
+    changedElement.className += ' framed';
 
-    // When the animation ends ...
-    var endAnimListen = function() {
-      // ... remove the classes I added:
-      if (changedElement.className) {
-        // TODO this may leave some whitespace in the class name
-        changedElement.className = changedElement.className.replace('animating', '');
-        changedElement.className = changedElement.className.replace(effect, '');
-        changedElement.className = changedElement.className.replace('framed', '');
-      }
-      // ... remove animation event listeners:
-      changedElement.removeEventListener('webkitAnimationStart', beginAnimListen);
-      changedElement.removeEventListener('webkitAnimationEnd', endAnimListen);
-    };
+  // When the animation starts ...
+  var beginAnimListen = function() {
+    // ... add a class to flag it as currently animating:
+    changedElement.className += changedElement.className ? ' animating': 'animating';
+  };
 
-    // Add animation event listeners:
-    // http://www.sitepoint.com/css3-animation-javascript-event-handlers/
-    changedElement.addEventListener('webkitAnimationStart', beginAnimListen, false);
-    changedElement.addEventListener('webkitAnimationEnd', endAnimListen, false);
-  });
+  // When the animation ends ...
+  var endAnimListen = function() {
+    // ... remove the classes I added:
+    if (changedElement.className) {
+      // TODO this may leave some whitespace in the class name
+      changedElement.className = changedElement.className.replace('animating', '');
+      changedElement.className = changedElement.className.replace(effect, '');
+      changedElement.className = changedElement.className.replace('framed', '');
+    }
+    // ... remove animation event listeners:
+    changedElement.removeEventListener('webkitAnimationStart', beginAnimListen);
+    changedElement.removeEventListener('webkitAnimationEnd', endAnimListen);
+  };
+
+  // Add animation event listeners:
+  // http://www.sitepoint.com/css3-animation-javascript-event-handlers/
+  changedElement.addEventListener('webkitAnimationStart', beginAnimListen, false);
+  changedElement.addEventListener('webkitAnimationEnd', endAnimListen, false);
+};
+
+var mutationReactor = function(summaries) {
+  // DOM element style updates:
+  var styleSummaries = summaries[0];
+  var styleReactor = reactorDispatch('style');
+  styleSummaries.valueChanged.forEach(styleReactor);
+  styleSummaries.added.forEach(styleReactor);
+  styleSummaries.removed.forEach(styleReactor);
+
+  // DOM elements added:
+  var elementSummaries = summaries[1];
+  var addReactor = reactorDispatch('add');
+  elementSummaries.added.forEach(addReactor);
+  elementSummaries.removed.forEach(addReactor);
+  elementSummaries.reparented.forEach(addReactor);
+
+  // CharacterData elements changed:
+  /*
+  var characterSummaries = summaries[2];
+  var characterReactor = reactorDispatch('character');
+  characterSummaries.valueChanged.forEach(characterReactor);
+  */
 };
 
 // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
 var observer = new MutationSummary({
-  callback: styleReactor,
+  callback: mutationReactor,
   queries: [
-    {attribute: 'style'}
+    {attribute: 'style'},
+    {element: '*'}
+    //{characterData: true}
   ]
 });
