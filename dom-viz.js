64adf866591ef074e827cd20a20945b7142dfd6c
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2015-08-25 12:50:44 -0400

    Fix bug leaving temp class names on highlighted elements. Fix indents.

--- dom-viz.js
+++ dom-viz.js
@@ -1,10 +1,10 @@
 var effect = 'highlight';
 
 var reactorDispatch = function(type) {
-	if (type == 'style') return highlightElement('framed');
-	else if (type == 'add') return highlightElement('framed-addremove');
-	//else if (type == 'fill') return highlightSVGElement;
-	else return null;
+    if (type == 'style') return highlightElement('framed');
+    else if (type == 'add') return highlightElement('framed-addremove');
+    //else if (type == 'fill') return highlightSVGElement;
+    else return null;
 };
 
 /*
@@ -16,93 +16,94 @@ var reactorDispatch = function(type) {
    */
 
 var highlightElement = function(type) {
-	return function(changedElement) {
-		/*
-		   if (changedElement.constructor.name == 'SVGRectElement') {
-		   console.log('[highlightElement] style = ' + changedElement.getAttribute('style'));
-		   console.log('[highlightElement] style fill = ' + changedElement.style.fill);
-		   console.log('[highlightElement] className = ' + changedElement.getAttribute('className'));
-		   console.log('[highlightElement] typeof changedElement = ' + changedElement.constructor.name);
-		   console.log('changedElement is an SVGRectElement');
-		   }
-		   */
-		// If I'm currently changing this element, don't do anything:
-		if (changedElement.getAttribute('className') && changedElement.className.match(/animating/))
-			return;
+    return function(changedElement) {
+        /*
+           if (changedElement.constructor.name == 'SVGRectElement') {
+           console.log('[highlightElement] style = ' + changedElement.getAttribute('style'));
+           console.log('[highlightElement] style fill = ' + changedElement.style.fill);
+           console.log('[highlightElement] className = ' + changedElement.getAttribute('className'));
+           console.log('[highlightElement] typeof changedElement = ' + changedElement.constructor.name);
+           console.log('changedElement is an SVGRectElement');
+           }
+           */
+        // If I'm currently changing this element, don't do anything:
+        if (changedElement.getAttribute('className') && changedElement.className.match(/animating/))
+            return;
 
-		// Add my css keyframes tag to the element's classes:
-		if (!changedElement.className.match(effect))
-			changedElement.className += changedElement.className ? ' ' + effect : effect;
-		// Add a solid border so my highlight animation can style it:
-		if (!changedElement.className.match(/framed.*/))
-		changedElement.className += ' ' + type;
+        // Add my css keyframes tag to the element's classes:
+        if (!changedElement.className.match(effect))
+            changedElement.className += changedElement.className ? ' ' + effect : effect;
+        // Add a solid border so my highlight animation can style it:
+        if (!changedElement.className.match(/framed.*/))
+            changedElement.className += ' ' + type;
 
-	// When the animation starts ...
-	var beginAnimListen = function() {
-		// ... add a class to flag it as currently animating:
-		changedElement.className += changedElement.className ? ' animating': 'animating';
-	};
+    // When the animation starts ...
+    var beginAnimListen = function() {
+        // ... add a class to flag it as currently animating:
+        if (!changedElement.className.match(/animating/))
+            changedElement.className += changedElement.className ? ' animating': 'animating';
+    };
 
-	// When the animation ends ...
-	var endAnimListen = function() {
-		// ... remove the classes I added:
-		if (changedElement.className) {
-			// TODO this may leave some whitespace in the class name
-			changedElement.className = changedElement.className.replace('animating', '');
-			changedElement.className = changedElement.className.replace(effect, '');
-			changedElement.className = changedElement.className.replace(type, '');
-		}
-		// ... remove animation event listeners:
-		changedElement.removeEventListener('webkitAnimationStart', beginAnimListen);
-		changedElement.removeEventListener('webkitAnimationEnd', endAnimListen);
-	};
+    // When the animation ends ...
+    var endAnimListen = function() {
+        // ... remove the classes I added:
+        if (changedElement.className) {
+            // TODO this may leave some whitespace in the class name
+            changedElement.className = changedElement.className.replace('animating', '');
+            changedElement.className = changedElement.className.replace(effect, '');
+            changedElement.className = changedElement.className.replace(type, '');
+        }
+        // ... remove animation event listeners:
+        changedElement.removeEventListener('webkitAnimationStart', beginAnimListen);
+        changedElement.removeEventListener('webkitAnimationEnd', endAnimListen);
+    };
 
-	// Add animation event listeners:
-	// http://www.sitepoint.com/css3-animation-javascript-event-handlers/
-	changedElement.addEventListener('webkitAnimationStart', beginAnimListen, false);
-	changedElement.addEventListener('webkitAnimationEnd', endAnimListen, false);
-	};
+    // Add animation event listeners:
+    // http://www.sitepoint.com/css3-animation-javascript-event-handlers/
+    changedElement.addEventListener('webkitAnimationStart', beginAnimListen, false);
+    changedElement.addEventListener('webkitAnimationEnd', endAnimListen, false);
+    };
 };
 
 var mutationReactor = function(summaries) {
 
-	// SVG element updates
-	/*
-	   var fillSummaries = summaries[0];
-	   var fillReactor = reactorDispatch('fill');
-	   fillSummaries.attributeChanged.style.forEach(fillReactor);
-	   */
+    // SVG element updates
+    /*
+       var fillSummaries = summaries[0];
+       var fillReactor = reactorDispatch('fill');
+       fillSummaries.attributeChanged.style.forEach(fillReactor);
+       */
 
-	// DOM element style updates:
-	var styleSummaries = summaries[1];
-	var styleReactor = reactorDispatch('style');
-	styleSummaries.valueChanged.forEach(styleReactor);
-	styleSummaries.added.forEach(styleReactor);
-	styleSummaries.removed.forEach(styleReactor);
+    // DOM element style updates:
+    var styleSummaries = summaries[1];
+    var styleReactor = reactorDispatch('style');
+    styleSummaries.valueChanged.forEach(styleReactor);
+    styleSummaries.added.forEach(styleReactor);
+    styleSummaries.removed.forEach(styleReactor);
 
 
-	// DOM elements added:
-	var elementSummaries = summaries[2];
-	var addReactor = reactorDispatch('add');
-	elementSummaries.added.forEach(addReactor);
-	elementSummaries.removed.forEach(addReactor);
-	elementSummaries.reparented.forEach(addReactor);
+    // DOM elements added:
+    var elementSummaries = summaries[2];
+    var addReactor = reactorDispatch('add');
+    elementSummaries.added.forEach(addReactor);
+    elementSummaries.removed.forEach(addReactor);
+    elementSummaries.reparented.forEach(addReactor);
 
-	// CharacterData elements changed:
-	/*
-	   var characterSummaries = summaries[2];
-	   var characterReactor = reactorDispatch('character');
-	   characterSummaries.valueChanged.forEach(characterReactor);
-	   */
+    // CharacterData elements changed:
+    /*
+       var characterSummaries = summaries[2];
+       var characterReactor = reactorDispatch('character');
+       characterSummaries.valueChanged.forEach(characterReactor);
+       */
 };
 
 // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
 var observer = new MutationSummary({
-	callback: mutationReactor,
-	queries: [
-		{element: 'rect', elementAttributes: 'style'},
-		{attribute: 'style'},
-		{element: '*'}
-		//{characterData: true}
-	]
+    callback: mutationReactor,
+    queries: [
+        {element: 'rect', elementAttributes: 'style'},
+        {attribute: 'style'},
+        {element: '*'}
+        //{characterData: true}
+    ]
 });
